c25b5f6ee290808f8979b4782da7b329dbc0f8ab
surya <suryachintu2001@gmai.com>
2020-08-04 13:34:20 +0530

    changed background colour

--- Planets/ContentView.swift
+++ Planets/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            Text("Hello, World!")
+                .foregroundColor(.white)
+        }
     }
 }
 
